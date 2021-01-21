0eca9b352138a2d80b4d1c017baaf2bd67a5757e
wriko.github.io <40769792+wriko@users.noreply.github.com>
2021-01-21 14:58:02 +0300

    использование переменных

--- sandbox/src/main/java/ru/stqa/softwaretesting/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/softwaretesting/sandbox/MyFirstProgram.java
@@ -3,7 +3,13 @@ package ru.stqa.softwaretesting.sandbox;
 public class MyFirstProgram {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World");
+        String somebady = "world";
+        System.out.println("Hello, " + somebady + "!");
+
+
+        int x = 8;
+        int s = x * x;
+        System.out.println("Площадь квадрата со стороной " + x + " = " + s);
     }
 
 }
