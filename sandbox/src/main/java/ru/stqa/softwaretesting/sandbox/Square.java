f2a6b3604e39808cc5db99794f7088c795f3a773
wriko.github.io <40769792+wriko@users.noreply.github.com>
2021-01-25 10:48:06 +0300

    добавлен импорт каталон-idea

--- sandbox/src/main/java/ru/stqa/softwaretesting/sandbox/Square.java
+++ sandbox/src/main/java/ru/stqa/softwaretesting/sandbox/Square.java
@@ -5,10 +5,12 @@ public class Square {
     public double l;
 
     public Square(double len) {
+
         this.l = len;
     }
 
     public double area() {
+
         return this.l * this.l;
     }
 
